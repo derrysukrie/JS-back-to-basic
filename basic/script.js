031f1698d94059e4181568c344412f07f48bb054
Derry <derry@woobiz.id>
2020-07-09 14:10:12 +0700

    first commit

---
+++ basic/script.js
@@ -0,0 +1,14 @@
+console.log('awdawdaw adkkaawkaw')
+ let name = 'derry'
+
+
+/**
+ * Type Coercion
+ */
+
+let firstName = 'Derry';
+let age = 20;
+
+console.log(age + ' ' + age); 
+ // hasilnya Derry 20, tapi age yang type datanya number ikut berubah jadi string
+ // karena type coercion
