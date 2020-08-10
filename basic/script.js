c3432101876fd3cdd1e0d408a214278c779c750e
derrysukrie <derrysukrie@gmail.com>
2020-08-10 19:21:02 +0700

    new cheatsheet

--- basic/script.js
+++ basic/script.js
@@ -12,3 +12,21 @@ let age = 20;
 console.log(age + ' ' + age); 
  // hasilnya Derry 20, tapi age yang type datanya number ikut berubah jadi string
  // karena type coercion
+
+
+//  Fitur Toped bulatkan tagihan
+
+var total = 150900
+Math.ceil(total / 1000) * 1000;
+
+// —> Hasil 151000
+
+// 151000 - 150900 = 100;  value nya adalah 100
+
+
+/**
+ *  Get Rid of duplicate value
+ */
+const arr = [1, 3, 4, 1, 2, 1]
+const newArr = [...new Set(arr)] 
+// [1, 3, 4, 2]
