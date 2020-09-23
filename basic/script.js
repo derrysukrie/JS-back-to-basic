7db2d0bde1239054a6fd8e0b9f5c052d05e0e8db
derrysukrie <derrysukrie@gmail.com>
2020-09-23 22:45:45 +0700

    divided string

--- basic/script.js
+++ basic/script.js
@@ -15,14 +15,12 @@ console.log(age + ' ' + age);
 
 
 //  Fitur Toped bulatkan tagihan
-
-var total = 150900
-Math.ceil(total / 1000) * 1000;
-
+// Math.ceil = Bulatkan ke atas eg: (1.4) diprint hasilnya 2
+var total = 150900;
+var amount = Math.ceil(total / 1000) * 1000;
 // —> Hasil 151000
-
 // 151000 - 150900 = 100;  value nya adalah 100
-
+console.log(amount)
 
 /**
  *  Get Rid of duplicate value
@@ -30,3 +28,14 @@ Math.ceil(total / 1000) * 1000;
 const arr = [1, 3, 4, 1, 2, 1]
 const newArr = [...new Set(arr)] 
 // [1, 3, 4, 2]
+
+
+// Divided a string into 2
+function divideStringTwo(str) {
+  let mid = Math.floor(str.length/2);
+  leftPart = str.substring(0, mid); // Get first array start from  index 0 to index 8;
+  rightPart = str.substring(mid); // Take the rest
+  return [leftPart, rightPart];
+}
+
+console.log(divideStringTwo('imgonnabedivided'))
